eb0e48282204a70ce95631c631647d762e267a8e
felle64 <felle641@gmail.com>
2024-02-15 18:01:31 +0100

    minor fixes

--- SLF1_Client/app/race-results/page.tsx
+++ SLF1_Client/app/race-results/page.tsx
@@ -6,9 +6,10 @@ export const metadata = {
 import React from 'react';
 
 const RaceResults: React.FC = () => {
+
   return (
     <div className='flex items-center pt-32 pb-12 md:pt-40 md:pb-20'>
-      <h1>Race Results</h1>
+      <h1 className='font-bold text-3xl pl-20'>Race Results</h1>
     </div>
   );
 };
