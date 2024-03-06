0ac5e910d37878f1f2d250af38e6cca1ae8ba60f
felle64 <felle641@gmail.com>
2024-03-06 16:39:35 +0100

    edits

--- SLF1_Client/components/hero.tsx
+++ SLF1_Client/components/hero.tsx
@@ -30,7 +30,7 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
-            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Sunday League <span className="bg-clip-text text-transparent bg-gradient-to-r from-black to-red-600">F1</span></h1>
+            <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Sunday League <span className="bg-clip-text text-transparent bg-gradient-to-r from-black to-red-600">F1 WIP</span></h1>
             <div className="max-w-3xl mx-auto">
               <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">Sunday League is an F1 23 Racing League, Join our discord today!</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
